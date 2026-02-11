07aa85653850e531de440315ff8e885314a3db8c
Sharad Chandel <sharadchandel2005@gmail.com>
2026-02-11 18:11:14 +0530

    fix: broken environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,51 @@
 
 export const environment = {
   production: false,
+  apiConfig: {
+    defaultLimit: 30,
+    maxLimit: 100,
+  },
+  providers: {
+    iconoir: {
+      path: 'iconoir',
+      displayName: 'Iconoir'
+    },
+    bootstrap: {
+      path: 'bootstrap',
+      displayName: 'Bootstrap'
+    },
+    feather: {
+      path: 'feather',
+      displayName: 'Feather'
+    },
+    gilbarbara: {
+      path: 'gilbarbara',
+      displayName: 'Gilbarbara'
+    },
+    heroicons: {
+      path: 'heroicons-24-solid',
+      displayName: 'Heroicons'
+    },
+    simpleIcons: {
+      path: 'simple-icons',
+      displayName: 'Simple Icons'
+    },
+    brandLogos: {
+      path: 'simple-svg-brand-logos',
+      displayName: 'Brand Logos'
+    },
+    tabler: {
+      path: 'tabler',
+      displayName: 'Tabler'
+    }
+  },
+  uiConfig: {
+    defaultProvider: 'ICONOIR',
+    gridColumns: {
+      xs: 2,
+      sm: 4,
+      md: 6,
+      lg: 8
+    }
+  }
 };
