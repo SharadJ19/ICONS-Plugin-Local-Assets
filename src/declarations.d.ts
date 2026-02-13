59009943d4d8f088ebbbe90e8272bda54191cc2c
Sharad Chandel <sharadchandel2005@gmail.com>
2026-02-13 10:28:03 +0530

    delete: redundant declarations

--- src/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-// src\declarations.d.ts
-
-declare module 'icons-plugin-standalone/QlPluginModule';
-declare module 'icons-plugin-standalone/QlDefaultPluginComponent';
-declare module 'icons-plugin-standalone/HomeComponent';
